f10cba91f59665a6dcdaa8efeb3a81960248f73c
davidsierrazu28 <dasierraz@unal.edu.co>
2023-04-05 09:00:19 -0500

    Solución

--- pregunta.py
+++ pregunta.py
@@ -13,9 +13,12 @@ import pandas as pd
 
 
 def ingest_data():
-
-    #
-    # Inserte su código aquí
-    #
-
-    return df
+    df_clusters = pd.read_fwf('clusters_report.txt',skiprows=4, names=['cluster','cantidad_de_palabras_clave','porcentaje_de_palabras_clave','principales_palabras_clave'])
+    df_clusters.fillna(method='ffill',inplace=True)
+    df_clusters = df_clusters.groupby(['cluster', 'cantidad_de_palabras_clave', 'porcentaje_de_palabras_clave'])['principales_palabras_clave'].apply(' '.join).reset_index()
+    df_clusters.principales_palabras_clave = df_clusters.principales_palabras_clave.str.split().apply(lambda x: " ".join(x))
+    df_clusters.principales_palabras_clave = df_clusters.principales_palabras_clave.str.replace(".", "", regex=False)
+    df_clusters.porcentaje_de_palabras_clave = df_clusters.porcentaje_de_palabras_clave.str.replace('%', '')
+    df_clusters.porcentaje_de_palabras_clave = df_clusters.porcentaje_de_palabras_clave.str.replace(',', '.')
+    df_clusters.porcentaje_de_palabras_clave = pd.to_numeric(df_clusters.porcentaje_de_palabras_clave)
+    return df_clusters
